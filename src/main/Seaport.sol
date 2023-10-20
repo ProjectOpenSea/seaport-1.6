28ec3aac9a05aa7e2f355bc346ac3daffc99be10
djviau <dan@opensea.io>
2023-10-20 13:32:46 -0400

    further cleanup

--- src/main/Seaport.sol
+++ src/main/Seaport.sol
@@ -1,11 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-import { Consideration as CoreConsideration } from
-    "seaport-core/src/lib/Consideration.sol";
+import { Seaport as CoreSeaport } from "seaport-core/src/Seaport.sol";
 
-contract Seaport is CoreConsideration {
-    constructor(address conduitController)
-        CoreConsideration(conduitController)
-    { }
+contract Seaport is CoreSeaport {
+    constructor(address conduitController) CoreSeaport(conduitController) { }
 }
